6d00fb4651bc720afa6699c8fca880551c5c6e82
Arman Rostami <62389951+armanrasta@users.noreply.github.com>
2024-09-15 00:44:23 +0330

    Delete KPIs directory

--- KPIs/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class KPIReportForm(forms.Form):
-    start_date = forms.DateField(widget=forms.SelectDateWidget)
-    end_date = forms.DateField(widget=forms.SelectDateWidget)
